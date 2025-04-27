6e741eddf7c57ed361e3641228f731857b821b85
Alex-Flexer <sasha.flexer@gmail.com>
2025-04-27 21:25:12 +0300

    fix style in shrotest_path.py

--- shortest_path.py
+++ shortest_path.py
@@ -4,8 +4,6 @@ from tools import calc_road_time
 from graph import Graph, Node
 
 
-
-
 def find_path(graph: Graph, start_idx: int, goal_idx: int) -> Iterable[Node]:
     def get_neighbors(node: Node) -> Iterable[Node]:
         return [graph[idx] for idx in node.output_roads.keys()]
